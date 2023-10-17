f52a71ff6f93ac4b5efa5344ae99861f8a10c65f
MOHAMMED NOORUDHEEN MT <117637149+NOOR-NOORZZZ@users.noreply.github.com>
2023-10-17 14:29:24 +0530

    Add files via upload

---
+++ CYCLE1/activty_lifecycle/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.sjcet.activity_lifecycle;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        notify("onCreate");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        notify("onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        notify("onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        notify("onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        notify("onDestroy");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        notify("onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        notify("onSaveInstanceState");
+    }
+
+    private void notify(String methodName) {
+        String name = this.getClass().getName();
+        String[] strings = name.split("\\.");
+        Toast.makeText(getApplicationContext(),
+                methodName + "" + strings[strings.length - 1],
+                Toast.LENGTH_LONG).show();
+    }
+
+}
